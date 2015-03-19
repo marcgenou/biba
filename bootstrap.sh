00f29cfb172d94c47e45b927c9e77c24cab83880
gon <gon@okachimachi.(none)>
2015-03-19 22:42:34 +0100

    fixed errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 apt-add-repository -y ppa:brightbox/ruby-ng
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
-'deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main' | sudo tee /etc/apt/sources.list.d/passenger.list
+echo 'deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main' | sudo tee /etc/apt/sources.list.d/passenger.list
 chown root: /etc/apt/sources.list.d/passenger.list
 chmod 600 /etc/apt/sources.list.d/passenger.list
 apt-get update
@@ -13,10 +13,11 @@ echo "<VirtualHost *:80>" > /etc/apache2/sites-available/testapp.conf
 echo "    ServerAdmin webmaster@localhost" >> /etc/apache2/sites-available/testapp.conf
 echo "    DocumentRoot /vagrant/biba/public" >> /etc/apache2/sites-available/testapp.conf
 echo "    RailsEnv development" >> /etc/apache2/sites-available/testapp.conf
-echo "    ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/testapp.conf
-echo "    CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/testapp.conf
+echo "    ErrorLog \${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/testapp.conf
+echo "    CustomLog \${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/testapp.conf
 echo "    <Directory "/vagrant/biba/public">" >> /etc/apache2/sites-available/testapp.conf
-echo "        Options FollowSymLinks" >> /etc/apache2/sites-available/testapp.conf
+echo "        Allow from all" >> /etc/apache2/sites-available/testapp.conf
+echo "        Options -MultiViews" >> /etc/apache2/sites-available/testapp.conf
 echo "        Require all granted" >> /etc/apache2/sites-available/testapp.conf
 echo "    </Directory>" >> /etc/apache2/sites-available/testapp.conf
 echo "</VirtualHost>" >> /etc/apache2/sites-available/testapp.conf
