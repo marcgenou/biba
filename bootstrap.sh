8c7bd5a0c9afeb53218b5f55c6010cf3e24cf549
gon <gon@okachimachi.(none)>
2015-03-20 17:04:30 +0100

    phpmyadmin added

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,14 +8,23 @@ chmod 600 /etc/apt/sources.list.d/passenger.list
 apt-get update
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password biba'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password biba'
-apt-get install -y build-essential libssl-dev libyaml-dev libreadline-dev openssl curl git-core zlib1g-dev bison libxml2-dev libxslt1-dev libcurl4-openssl-dev libsqlite3-dev ruby2.2 ruby2.2-dev curl git libmysqlclient-dev mysql-server nodejs apache2 libapache2-mod-passenger
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password biba" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password biba" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password biba" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+apt-get install -y build-essential libssl-dev libyaml-dev libreadline-dev openssl curl git-core zlib1g-dev bison libxml2-dev libxslt1-dev libcurl4-openssl-dev libsqlite3-dev ruby2.2 ruby2.2-dev curl git libmysqlclient-dev mysql-server nodejs apache2 libapache2-mod-passenger phpmyadmin
 echo "<VirtualHost *:80>" > /etc/apache2/sites-available/testapp.conf
 echo "    ServerAdmin webmaster@localhost" >> /etc/apache2/sites-available/testapp.conf
 echo "    DocumentRoot /vagrant/biba/public" >> /etc/apache2/sites-available/testapp.conf
+echo "    <Directory /usr/share/phpmyadmin>" >> /etc/apache2/sites-available/testapp.conf
+echo "        PassengerEnabled off" >> /etc/apache2/sites-available/testapp.conf
+echo "        AllowOverride all" >> /etc/apache2/sites-available/testapp.conf
+echo "    </Directory>" >> /etc/apache2/sites-available/testapp.conf
 echo "    RailsEnv development" >> /etc/apache2/sites-available/testapp.conf
 echo "    ErrorLog \${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/testapp.conf
 echo "    CustomLog \${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/testapp.conf
-echo "    <Directory "/vagrant/biba/public">" >> /etc/apache2/sites-available/testapp.conf
+echo "    <Directory /vagrant/biba/public>" >> /etc/apache2/sites-available/testapp.conf
 echo "        Allow from all" >> /etc/apache2/sites-available/testapp.conf
 echo "        Options -MultiViews" >> /etc/apache2/sites-available/testapp.conf
 echo "        Require all granted" >> /etc/apache2/sites-available/testapp.conf
@@ -23,10 +32,11 @@ echo "    </Directory>" >> /etc/apache2/sites-available/testapp.conf
 echo "</VirtualHost>" >> /etc/apache2/sites-available/testapp.conf
 a2dissite 000-default
 a2ensite testapp
+php5enmod mcrypt
 service apache2 restart
-mysql -u root -pbiba -e "create database biba_development CHARACTER SET utf8 COLLATE utf8_general_ci;"
-mysql -u root -pbiba -e "create database biba_test CHARACTER SET utf8 COLLATE utf8_general_ci;"
-mysql -u root -pbiba -e "create database biba_production CHARACTER SET utf8 COLLATE utf8_general_ci;"
+#mysql -u root -pbiba -e "create database biba_development CHARACTER SET utf8 COLLATE utf8_general_ci;"
+#mysql -u root -pbiba -e "create database biba_test CHARACTER SET utf8 COLLATE utf8_general_ci;"
+#mysql -u root -pbiba -e "create database biba_production CHARACTER SET utf8 COLLATE utf8_general_ci;"
 gem install rails --version 4.2.0 --no-ri --no-rdoc
 cd /vagrant/biba
 bundle install
